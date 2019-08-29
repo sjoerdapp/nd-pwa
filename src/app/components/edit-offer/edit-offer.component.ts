bd5c56aab8b6c7e5f0a7d445fc44c77933a0c083
momar <momar@nxtdrop.com>
2019-08-28 23:43:51 -0600

    edit offer: update and delete

--- src/app/components/edit-offer/edit-offer.component.ts
+++ src/app/components/edit-offer/edit-offer.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProfileService } from 'src/app/services/profile.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-offer',
@@ -13,9 +14,19 @@ export class EditOfferComponent implements OnInit {
 
   offerInfo;
 
+  conditionChanged = false;
+  priceChanged = false;
+  sizeChanged = false;
+
+  curCondition;
+  curPrice;
+  curSize;
+
   constructor(
     private route: ActivatedRoute,
-    private profileService: ProfileService
+    private profileService: ProfileService,
+    private ngZone: NgZone,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -25,8 +36,68 @@ export class EditOfferComponent implements OnInit {
         this.offerInfo = data;
         (document.getElementById('item-size') as HTMLInputElement).value = this.offerInfo.size;
         (document.getElementById('radio-' + this.offerInfo.condition) as HTMLInputElement).checked = true;
+        this.curCondition = this.offerInfo.condition;
+        this.curPrice = this.offerInfo.price;
+        this.curSize = this.offerInfo.size;
       });
     });
+  }
+
+  conditionChanges($event) {
+    if (this.offerInfo.condition != $event.target.value && this.conditionChanged == false) {
+      this.conditionChanged = true;
+    } else if (this.offerInfo.condition == $event.target.value && this.conditionChanged == true) {
+      this.conditionChanged = false;
+    }
+
+    this.curCondition = $event.target.value;
+  }
+
+  priceChanges($event) {
+    if (this.offerInfo.price != $event.target.value && this.priceChanged == false) {
+      this.priceChanged = true;
+    } else if ((this.offerInfo.price == $event.target.value || $event.target.value == '') && this.priceChanged == true) {
+      this.priceChanged = false;
+    }
+
+    this.curPrice = $event.target.value;
+  }
+
+  sizeChanges($event) {
+    if (this.offerInfo.size != $event.target.value && this.sizeChanged == false) {
+      this.sizeChanged = true;
+    } else if (this.offerInfo.size == $event.target.value && this.sizeChanged == true) {
+      this.sizeChanged = false;
+    }
+
+    this.curSize = $event.target.value;
+  }
+
+  updateListing() {
+    const condition = this.curCondition;
+    const price = this.curPrice;
+    const size = this.curSize;
+
+    if (this.conditionChanged || this.priceChanged || this.sizeChanged) {
+      this.profileService.updateOffer(this.offerInfo.listingID, this.offerInfo.productID, condition, price, size).then((res) => {
+        if (res) {
+          return this.ngZone.run(() => {
+            return this.router.navigate(['../../profile']);
+          });
+        }
+      });
+    }
+  }
+
+  deleteListing() {
+    this.profileService.deleteOffer(this.offerInfo.listingID, this.offerInfo.productID)
+      .then((res) => {
+        if (res) {
+          return this.ngZone.run(() => {
+            return this.router.navigate(['../../profile']);
+          });
+        }
+      });
   }
 
 }
--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -3,6 +3,7 @@ import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection 
 import { AuthService } from './auth.service';
 import { User } from '../models/user';
 import { Observable } from 'rxjs';
+import * as firebase from 'firebase/app';
 
 @Injectable({
   providedIn: 'root'
@@ -43,5 +44,68 @@ export class ProfileService {
 
     const offerRef: AngularFirestoreDocument<any> = this.afs.collection('users').doc(`${UID}`).collection('listings').doc(`${listingID}`);
     return offerRef.valueChanges();
+  }
+
+  public async updateOffer(listingID, productID, condition, price, size): Promise<boolean> {
+    let UID: string;
+    await this.auth.isConnected().then(data => {
+      UID = data.uid;
+    });
+
+    const batch = this.afs.firestore.batch();
+
+    const offerRef = this.afs.firestore.collection('users').doc(`${UID}`).collection('listings').doc(`${listingID}`);
+    const prodRef = this.afs.firestore.collection('products').doc(`${productID}`).collection('listings').doc(`${listingID}`);
+
+    batch.update(offerRef, {
+      condition: condition,
+      price: price,
+      size: size
+    });
+
+    batch.update(prodRef, {
+      condition: condition,
+      price: price,
+      size: size
+    });
+
+    return batch.commit()
+      .then(() => {
+        console.log('Listing updated');
+        return true;
+      })
+      .catch((err) => {
+        console.error(err);
+        return false;
+      });
+  }
+
+  public async deleteOffer(listingID, productID): Promise<boolean> {
+    let UID: string;
+    await this.auth.isConnected().then(data => {
+      UID = data.uid;
+    });
+
+    const batch = this.afs.firestore.batch();
+
+    const offerRef = this.afs.firestore.collection('users').doc(`${UID}`).collection('listings').doc(`${listingID}`);
+    const prodRef = this.afs.firestore.collection('products').doc(`${productID}`).collection('listings').doc(`${listingID}`);
+    const userRef = this.afs.firestore.collection('users').doc(`${UID}`);
+
+    batch.delete(offerRef);
+    batch.delete(prodRef);
+    batch.update(userRef, {
+      listed: firebase.firestore.FieldValue.increment(-1)
+    });
+
+    return batch.commit()
+      .then(() => {
+        console.log('listing deleted');
+        return true;
+      })
+      .catch((err) => {
+        console.error(err);
+        return false;
+      });
   }
 }
