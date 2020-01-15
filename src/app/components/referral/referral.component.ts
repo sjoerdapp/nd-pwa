721e949f6d3b1f55ab75ee39d6649c6a93f6f14e
momar <momar@nxtdrop.com>
2020-01-14 17:47:32 -0700

    responsive design referral

--- src/app/components/referral/referral.component.ts
+++ src/app/components/referral/referral.component.ts
@@ -4,6 +4,7 @@ import { isNullOrUndefined } from 'util';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-referral',
@@ -20,7 +21,8 @@ export class ReferralComponent implements OnInit {
   constructor(
     private auth: AuthService,
     private http: HttpClient,
-    private afs: AngularFirestore
+    private afs: AngularFirestore,
+    private routre: Router
   ) { }
 
   ngOnInit() {
@@ -33,37 +35,36 @@ export class ReferralComponent implements OnInit {
 
     this.auth.isConnected().then(res => {
 
-      if (isNullOrUndefined(res)) {
+      if (isNullOrUndefined(res) || !isNaN(this.phoneNumber) || !isNullOrUndefined(this.phoneNumber)) {
         this.loading = false;
         this.error = true;
       } else {
-        if (!isNaN(this.phoneNumber) && !isNullOrUndefined(this.phoneNumber)) {
-          this.afs.collection(`users`).doc(`${res.uid}`).get().subscribe(response => {
-            let name;
-            (isNullOrUndefined(response.data().lastName)) ? name = response.data().firstName + response.data().lastName : name = response.data().firstName;
+        this.afs.collection(`users`).doc(`${res.uid}`).get().subscribe(response => {
+          let name;
+          (isNullOrUndefined(response.data().lastName)) ? name = response.data().firstName + response.data().lastName : name = response.data().firstName;
 
-            const data = {
-              phoneNumber: this.phoneNumber,
-              name
+          const data = {
+            phoneNumber: this.phoneNumber,
+            name
+          }
+
+          this.http.post(`${environment.cloud.url}inviteFriend`, data).subscribe(message => {
+            this.loading = false;
+
+            if (message) {
+              this.sent = true;
+              this.routre.navigate(['..']);
+            } else {
+              this.error = true;
             }
-
-            this.http.post(`${environment.cloud.url}inviteFriend`, data).subscribe(message => {
-              this.loading = false;
-
-              if (message) {
-                this.sent = true;
-              } else {
-                this.error = true;
-              }
-            })
-          });
-        }
-
-        setTimeout(() => {
-          this.error = false;
-          this.sent = false;
-        }, 1500)
+          })
+        });
       }
+
+      setTimeout(() => {
+        this.error = false;
+        this.sent = false;
+      }, 2000)
     });
   }
 
